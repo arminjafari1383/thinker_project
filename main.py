52be32012f24abc16f71e68b813438473c965321
armin <arminjafri138386@gmail.com>
2024-07-15 04:15:42 +0330

    learn python tkiner

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from tkinter import *
 root = Tk()
-myLabel = Label(root,text="hello world")
-myLabel.pack()
+#creating alabel widget 
+myLabel1 = Label(root,text="hello world")
+myLabel2 = Label(root,text="my name is john elder ")
+myLabel1.grid(row=0,column=0)
+myLabel2.grid(row=1,column=5)
+
 root.mainloop()
