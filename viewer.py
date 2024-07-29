745bd32e32e611e13ceb2f6121fb0e8df22fd563
armin <arminjafri138386@gmail.com>
2024-07-30 02:16:29 +0330

    create status bar

--- viewer.py
+++ viewer.py
@@ -7,14 +7,16 @@ root = Tk()
 root.title('learn to codemy')
 root.iconbitmap('images.jpg')
 my_img1 = ImageTk.PhotoImage(Image.open("images.jpg"))
-my_img2 = ImageTk.PhotoImage(Image.open("viewer/photo_2024-05-25_19-50-56.jpg"))
+my_img2 = ImageTk.PhotoImage(Image.open("images.jpg"))
 my_img3 = ImageTk.PhotoImage(Image.open("images.jpg"))
-my_img4 = ImageTk.PhotoImage(Image.open("viewer/photo_2024-05-25_19-50-56.jpg"))
+my_img4 = ImageTk.PhotoImage(Image.open("images.jpg"))
 my_img5 = ImageTk.PhotoImage(Image.open("images.jpg"))
-my_img6 = ImageTk.PhotoImage(Image.open("viewer/photo_2024-05-25_19-50-56.jpg"))
+my_img6 = ImageTk.PhotoImage(Image.open("images.jpg"))
 
 
 image_list = [my_img1,my_img2,my_img3,my_img4,my_img5,my_img6]
+status = Label(root,text = "Image 1 of " + str(len(image_list) - 1), bd=1, relief=SUNKEN,anchor=E)
+
 
 my_label = Label(image=my_img1)
 my_label.grid(row = 0,column = 0,columnspan = 3)
@@ -32,7 +34,8 @@ def forward(image_number):
     my_label.grid(row = 0,column = 0,columnspan = 3)
     button_back.grid(row = 0, column = 0,columnspan=3)
     button_forward.grid(row = 1, column=2)
-
+    status = Label(root,text = "Image " +str(image_number) + " of " + str(len(image_list) - 1), bd=1, relief=SUNKEN,anchor=E)
+    status.grid(row = 2, column = 0 ,columnspan = 3,sticky= W + E)
 
 def back(image_number):
     global my_label
@@ -47,11 +50,17 @@ def back(image_number):
     my_label.grid(row = 0,column = 0,columnspan = 3)
     button_back.grid(row = 0, column = 0,columnspan=3)
     button_forward.grid(row = 1, column=2)
+    status = Label(root,text = "Image " +str(image_number) + " of " + str(len(image_list) - 1), bd=1, relief=SUNKEN,anchor=E)
+    status.grid(row = 2, column = 0 ,columnspan = 3,sticky= W + E)
 button_back = Button(root, text="<<",command = back,state = DISABLED)
 button_exit = Button(root, text="EXIT PROGRAM",command = root.quit)
 button_forward = Button(root, text=">>",command = lambda: forward(2))
 button_back.grid(row = 1,column = 0)
 button_exit.grid(row = 1,column = 1)
 button_forward.grid(row = 1,column = 2)
+status.grid(row = 2, column = 0 ,columnspan = 3,sticky= W + E)
+
+
+
 
 root.mainloop()
